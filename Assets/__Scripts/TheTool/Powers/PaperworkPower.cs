5cc05a9db2ee6308e1f9eed431bf0976303378ed
Biurito <oto.wojtek@gmail.com>
2024-02-24 18:10:22 +0100

    Paperwork power

---
+++ Assets/__Scripts/TheTool/Powers/PaperworkPower.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using GordonEssentials;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PaperworkPower", menuName = "Power/PaperworkPower")]
+public class PaperworkPower : Power
+{
+    [SerializeField]
+    private GameObject paperworkReference;
+
+    public override void UsePower()
+    {
+        GameObject paperWork = Instantiate(paperworkReference);
+        paperWork.transform.position = Player.Instance.paperworkPivot.transform.position;
+        paperWork.transform.rotation = Player.Instance.paperworkPivot.transform.rotation;
+
+        paperWork.GetComponent<Rigidbody>().AddForce(paperWork.transform.forward * 5 , ForceMode.Impulse);
+
+        Debug.Log("Paperwork Power");
+    }
+}
+

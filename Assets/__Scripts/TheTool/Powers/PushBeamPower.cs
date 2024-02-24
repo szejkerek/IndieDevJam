e57afb603499155df781cbc9f8fc77f67189c0f7
Bartek Gordon <bartekk.gordon@gmail.com>
2024-02-24 13:04:53 +0100

    Import The Tool

---
+++ Assets/__Scripts/TheTool/Powers/PushBeamPower.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBeamPower : IPower
+{
+    public void UsePower()
+    {
+        Debug.Log("Push Beam");
+    }
+}

d947e1318407806482c220b8d56f147ba4dffe16
Bartek Gordon <bartekk.gordon@gmail.com>
2024-02-24 15:30:14 +0100

    Get player look rotation

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using GordonEssentials;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Singleton<Player>
+{
+    [SerializeField] Camera playerCamera;
+
+
+    public Vector3 GetLookRotation()
+    {
+        if (playerCamera == null)
+        {
+            Debug.LogError("Player camera is not assigned!");
+            return Vector3.zero;
+        }
+
+        Vector3 lookRotation = playerCamera.transform.forward;
+        return lookRotation;
+    }
+
+    private void Update()
+    {
+        Debug.DrawLine(playerCamera.transform.position, GetLookRotation());
+    }
+}

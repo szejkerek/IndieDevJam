8711a780430539a776a00be349c889802a279fc1
Biurito <oto.wojtek@gmail.com>
2024-02-24 15:26:20 +0100

    Freeze

--- Assets/__Scripts/TheTool/Powers/FreezePower.cs
+++ Assets/__Scripts/TheTool/Powers/FreezePower.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FreezePower", menuName = "Power/FreezePower")]
 public class FreezePower : Power
 {
+    private float radius = 10f;
     public override void UsePower()
     {
+        Collider[] hitColliders = Physics.OverlapSphere(TheTool.Instance.transform.position, radius);
+        foreach (var hitCollider in hitColliders)
+        {
+            if(hitCollider.gameObject.GetComponent<Rigidbody>() != null)
+            {
+                Debug.Log(hitCollider.gameObject.name);
+                Rigidbody rb = hitCollider.gameObject.GetComponent<Rigidbody>();
+                rb.isKinematic = !rb.isKinematic;
+                //rb.useGravity = !rb.useGravity;
+            }
+        }
         Debug.Log("Freeze Power");
     }
 }

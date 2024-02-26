9deed386327720963ca9ea7c98ba93e46800bdc5
Bartek Gordon <bartekk.gordon@gmail.com>
2024-02-26 15:12:11 +0100

    Main menu

--- Assets/__Scripts/Managers/MainMenuManager.cs
+++ Assets/__Scripts/Managers/MainMenuManager.cs
@@ -1,21 +1,41 @@
 using GordonEssentials;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using System;
+using GordonEssentials.Types;
 
 public class MainMenuManager : Singleton<MainMenuManager>
 {
     [SerializeField] Button StartGameBtn;
     [SerializeField] Button QuitGameBtn;
 
+    [SerializeField] Sound clickSound; // Add your click sound here
+    private AudioSource audioSource;
+
     void Start()
     {
-        StartGameBtn.onClick.AddListener(() => StartGame());
-        QuitGameBtn.onClick.AddListener(() => Application.Quit());
+        audioSource = GetComponent<AudioSource>();
+
+        StartGameBtn.onClick.AddListener(() => OnButtonClick(StartGameBtn, () => StartGame()));
+        QuitGameBtn.onClick.AddListener(() => OnButtonClick(QuitGameBtn, () => Application.Quit()));
     }
 
+    private void OnButtonClick(Button obj, Action onClickAction)
+    {
+        AudioManager.Instance.PlayGlobal(clickSound);
+
+        // Button animation using DOTween
+        obj.transform.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 0.2f)
+            .OnComplete(() =>
+            {
+                obj.transform.DOScale(Vector3.one, 0.2f);
+                // Execute the click action after the animation completes
+                onClickAction?.Invoke();
+            });
+    }
     private void StartGame()
     {
         SceneLoader.Instance.LoadScene(SceneConstants.Gameplay);
